a1a9a3ba6dff7095a46a726330a301e099dbbf64
Jusserand Romain <Jusserand Romain>
2016-11-04 15:07:20 +0100

    fix gulp task

--- gulp-tasks/build.js
+++ gulp-tasks/build.js
@@ -2,45 +2,44 @@
 
 var gulp = require('gulp'),
     browserSync = require('browser-sync'),
-        sass = require('gulp-sass'),
+    sass = require('gulp-sass'),
     sourcemaps = require('gulp-sourcemaps'),
     postcss = require('gulp-postcss');
 var jshint = require('gulp-jshint');
 var uglify = require('gulp-uglify');
-var minifyCSS = require('gulp-minify-css');
+var minifyCSS = require('gulp-cssnano');
 var clean = require('gulp-clean');
 var runSequence = require('run-sequence');
 
 // tasks
 gulp.task('lint', function() {
-  gulp.src(['./app/**/*.js', '!./app/bower_components/**'])
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'))
-    .pipe(jshint.reporter('fail'));
+    gulp.src(['./app/**/*.js', '!./app/bower_components/**'])
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'))
+        .pipe(jshint.reporter('fail'));
 });
 
 gulp.task('minify-css', function() {
-  var opts = {comments:true,spare:true};
-  gulp.src(['./app/**/*.css', '!./app/bower_components/**'])
-    .pipe(minifyCSS(opts))
-    .pipe(gulp.dest('./dist/'))
+    var opts = { comments: true, spare: true };
+    gulp.src(['./app/**/*.css', '!./app/bower_components/**'])
+        .pipe(minifyCSS(opts))
+        .pipe(gulp.dest('./dist/'))
 });
 gulp.task('minify-js', function() {
-  gulp.src(['./app/**/*.js', '!./app/bower_components/**'])
-    .pipe(uglify({
-      // inSourceMap:
-      // outSourceMap: "app.js.map"
-    }))
-    .pipe(gulp.dest('./dist/'))
+    gulp.src(['./app/**/*.js', '!./app/bower_components/**'])
+        .pipe(uglify({
+            // inSourceMap:
+            // outSourceMap: "app.js.map"
+        }))
+        .pipe(gulp.dest('./dist/'))
 });
-gulp.task('copy-bower-components', function () {
-  gulp.src('./app/bower_components/**')
-    .pipe(gulp.dest('dist/bower_components'));
+gulp.task('copy-bower-components', function() {
+    gulp.src('./app/bower_components/**')
+        .pipe(gulp.dest('dist/bower_components'));
 });
 
 gulp.task('build', function() {
-  runSequence(
-    ['clean'],
-    ['lint', 'minify-css', 'minify-js', 'sass-build', 'htmlmin', 'copy-bower-components', 'serve-prod']
-  );
+    runSequence(
+        ['clean'], ['lint', 'minify-css', 'minify-js', 'sass-build', 'htmlmin', 'copy-bower-components', 'serve-prod']
+    );
 });
--- gulp-tasks/htmlmin.js
+++ gulp-tasks/htmlmin.js
@@ -4,25 +4,25 @@ var gulp = require('gulp'),
     gulpif = require('gulp-if'),
     useref = require('gulp-useref'),
     uglify = require('gulp-uglify'),
-    minifyCss = require('gulp-minify-css'),
+    minifyCss = require('gulp-cssnano'),
     minifyHTML = require('gulp-minify-html');
 
 var config = require('./config');
 
-gulp.task('htmlmin', ['wiredep', 'sass'], function () {
-  var assets = useref.assets();
+gulp.task('htmlmin', ['wiredep', 'sass'], function() {
+    var assets = useref.assets();
 
-  var opts = {
-    conditionals: true,
-    loose: true
-  };
+    var opts = {
+        conditionals: true,
+        loose: true
+    };
 
-  return gulp.src(config.src + '**/*.html')
-    .pipe(assets)
-    .pipe(gulpif('*.js', uglify()))
-    .pipe(gulpif('*.css', minifyCss({compatibility: 'ie7,ie8,*,+units.rem'})))
-    .pipe(assets.restore())
-    .pipe(useref())
-    .pipe(gulpif('*.html', minifyHTML(opts)))
-    .pipe(gulp.dest(config.dist));
+    return gulp.src(config.src + '**/*.html')
+        .pipe(assets)
+        .pipe(gulpif('*.js', uglify()))
+        .pipe(gulpif('*.css', minifyCss({ compatibility: 'ie7,ie8,*,+units.rem' })))
+        .pipe(assets.restore())
+        .pipe(useref())
+        .pipe(gulpif('*.html', minifyHTML(opts)))
+        .pipe(gulp.dest(config.dist));
 });
